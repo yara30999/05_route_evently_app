31895da00bc585b729fdc0d99b45ffce5af3b760
yara30999 <yaranaserelniny@gmail.com>
2025-01-13 23:50:21 +0200

    remove splash screen after delay

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'app/app_prefs.dart';
 import 'presentation/01_introduction_screen/view_model/theme_provider.dart';
+import 'presentation/resourses/constants_manager.dart';
 import 'presentation/resourses/language_manager.dart';
 import 'presentation/resourses/routes_manager.dart';
 import 'presentation/resourses/theme_manager.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await EasyLocalization.ensureInitialized();
   SharedPreferences sharedprefs = await SharedPreferences.getInstance();
+  Future.delayed(Duration(seconds: AppConstants.splashDelay))
+      .then((value) => {FlutterNativeSplash.remove()});
   runApp(
     EasyLocalization(
         supportedLocales: [
--- lib/presentation/resourses/constants_manager.dart
+++ lib/presentation/resourses/constants_manager.dart
@@ -1,4 +1,5 @@
 class AppConstants {
+  static const int splashDelay = 2;
   static const int breakPointMobileTablet = 800; //1100
   static const int breakPointTabletLaptop = 1200; //1700
 }
