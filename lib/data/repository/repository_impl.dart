af1e6bed5e4c304c0b0cb4b2d22b201adf2b4d8c
yara30999 <yaranaserelniny@gmail.com>
2025-01-15 04:27:31 +0200

    repository implementer for auth functions

--- lib/data/repository/repository_impl.dart
+++ lib/data/repository/repository_impl.dart
@@ -1,1 +1,105 @@
+import 'package:dartz/dartz.dart';
+import '../../app/app_prefs.dart';
+import '../../domain/entities/auth_entity.dart';
+import '../../domain/repository/repository.dart';
+import '../data_source/local_data_source.dart';
+import '../data_source/remote_data_source.dart';
+import '../network/error_handler.dart';
+import '../network/failure.dart';
+import '../network/network_info.dart';
+import '../network/requests.dart';
 
+class RepositoryImpl implements Repository {
+  final RemoteDataSource _remoteDataSource;
+  final LocalDataSource _localDataSource;
+  final NetworkInfo _networkInfo;
+  final AppPreferences _appPreferences;
+  RepositoryImpl(this._remoteDataSource, this._networkInfo,
+      this._localDataSource, this._appPreferences);
+
+  @override
+  Future<Either<Failure, AuthenticationEntity>> login(
+      LoginRequest loginRequest) async {
+    if (await _networkInfo.isConnected) {
+      // its connected to internet, its safe to call API
+      try {
+        final AuthenticationEntity authenticationEntity =
+            await _remoteDataSource.login(loginRequest);
+        await _localDataSource.saveUserDataToCache(authenticationEntity);
+        await _appPreferences.setUserLoggedIn();
+        return Right(authenticationEntity);
+      } catch (error) {
+        return Left(ErrorHandler.handle(error).failure);
+      }
+    } else {
+      // return internet connection error
+      // return either left
+      return Left(DataSource.noInternetConnection.getFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, AuthenticationEntity>> register(
+      RegisterRequest registerRequest) async {
+    if (await _networkInfo.isConnected) {
+      try {
+        final AuthenticationEntity authenticationEntity =
+            await _remoteDataSource.register(registerRequest);
+        await _localDataSource.saveUserDataToCache(authenticationEntity);
+        await _appPreferences.setUserLoggedIn();
+        return Right(authenticationEntity);
+      } catch (error) {
+        return Left(ErrorHandler.handle(error).failure);
+      }
+    } else {
+      return Left(DataSource.noInternetConnection.getFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, AuthenticationEntity>> googleSignIn() async {
+    if (await _networkInfo.isConnected) {
+      try {
+        final AuthenticationEntity authenticationEntity =
+            await _remoteDataSource.googleSignIn();
+        await _localDataSource.saveUserDataToCache(authenticationEntity);
+        await _appPreferences.setUserLoggedIn();
+        return Right(authenticationEntity);
+      } catch (error) {
+        return Left(ErrorHandler.handle(error).failure);
+      }
+    } else {
+      return Left(DataSource.noInternetConnection.getFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, bool>> logout() async {
+    if (await _networkInfo.isConnected) {
+      try {
+        await _remoteDataSource.logout();
+        await _localDataSource.clearAllCachedBoxes();
+        await _appPreferences.removePrefs();
+        return const Right(true);
+      } catch (error) {
+        return Left(ErrorHandler.handle(error).failure);
+      }
+    } else {
+      return Left(DataSource.noInternetConnection.getFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, bool>> forgotPassword(String email) async {
+    if (await _networkInfo.isConnected) {
+      try {
+        await _remoteDataSource.forgotPassword(email);
+        return const Right(true);
+      } catch (error) {
+        return Left(ErrorHandler.handle(error).failure);
+      }
+    } else {
+      return Left(DataSource.noInternetConnection.getFailure());
+    }
+  }
+}
--- lib/domain/repository/repository.dart
+++ lib/domain/repository/repository.dart
@@ -1,1 +1,14 @@
+import 'package:dartz/dartz.dart';
+import '../../data/network/failure.dart';
+import '../../data/network/requests.dart';
+import '../entities/auth_entity.dart';
 
+abstract class Repository {
+  Future<Either<Failure, AuthenticationEntity>> login(
+      LoginRequest loginRequest);
+  Future<Either<Failure, bool>> forgotPassword(String email);
+  Future<Either<Failure, AuthenticationEntity>> register(
+      RegisterRequest registerRequest);
+  Future<Either<Failure, AuthenticationEntity>> googleSignIn();
+  Future<Either<Failure, bool>> logout();
+}
