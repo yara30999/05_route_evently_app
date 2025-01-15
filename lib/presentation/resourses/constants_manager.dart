f67dff3d58a4ef135f8b80bfa53ba5e4a1833b00
yara30999 <yaranaserelniny@gmail.com>
2025-01-15 03:11:54 +0200

    setup error handler

--- lib/presentation/resourses/constants_manager.dart
+++ lib/presentation/resourses/constants_manager.dart
@@ -2,4 +2,6 @@ class AppConstants {
   static const int splashDelay = 2;
   static const int breakPointMobileTablet = 800; //1100
   static const int breakPointTabletLaptop = 1200; //1700
+  static const String google = 'google';
+  static const String facebook = 'facebook';
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import cloud_firestore
+import connectivity_plus
 import firebase_auth
 import firebase_core
 import google_sign_in_ios
@@ -13,6 +14,7 @@ import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
