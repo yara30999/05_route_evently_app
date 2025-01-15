130b8e7b6710a37fcb5d2efcc0e4983af04480f2
yara30999 <yaranaserelniny@gmail.com>
2025-01-15 02:49:56 +0200

    structure project for clean architecture

---
+++ lib/domain/entities/auth_entity.dart
@@ -0,0 +1,11 @@
+class AuthenticationEntity {
+  final String uid;
+  final String name;
+  final String email;
+
+  const AuthenticationEntity({
+    required this.uid,
+    required this.name,
+    required this.email,
+  });
+}
