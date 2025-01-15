2cb918f124743c7b919365b4344305f381b759d7
yara30999 <yaranaserelniny@gmail.com>
2025-01-15 04:58:35 +0200

    setup dependency injection

--- lib/app/di.dart
+++ lib/app/di.dart
@@ -1,5 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import '../data/data_source/local_data_source.dart';
+import '../data/data_source/remote_data_source.dart';
+import '../data/network/network_info.dart';
+import '../data/repository/repository_impl.dart';
+import '../domain/repository/repository.dart';
+import '../domain/usecase/forgot_password_usecase.dart';
+import '../domain/usecase/google_sign_in_usecase.dart';
+import '../domain/usecase/login_usecase.dart';
+import '../domain/usecase/logout_usecase.dart';
+import '../domain/usecase/register_usecase.dart';
 import 'app_prefs.dart';
 import 'validation_service.dart';
 
@@ -14,4 +26,40 @@ Future<void> initAppModule() async {
   // validation service instance for all text form fields in the app
   instance
       .registerLazySingleton<IValidationService>(() => ValidationServiceImpl());
+  // network info
+  instance.registerLazySingleton<NetworkInfo>(
+      () => NetworkInfoImpl(InternetConnectionChecker.instance));
+  // remote data source
+  final firebaseInstance = FirebaseAuth.instance;
+  instance.registerLazySingleton<FirebaseAuth>(() => firebaseInstance);
+  instance.registerLazySingleton<RemoteDataSource>(
+      () => RemoteDataSourceImpl(instance<FirebaseAuth>()));
+  // local data source
+  instance.registerLazySingleton<LocalDataSource>(() => LocalDataSourceImpl());
+  // repository
+  instance.registerLazySingleton<Repository>(
+      () => RepositoryImpl(instance(), instance(), instance(), instance()));
+  // login usecase
+  if (!GetIt.I.isRegistered<LoginUseCase>()) {
+    instance.registerFactory<LoginUseCase>(() => LoginUseCase(instance()));
+  }
+  // register usecase
+  if (!GetIt.I.isRegistered<RegisterUsecase>()) {
+    instance
+        .registerFactory<RegisterUsecase>(() => RegisterUsecase(instance()));
+  }
+  // logout usecase
+  if (!GetIt.I.isRegistered<LogoutUsecase>()) {
+    instance.registerFactory<LogoutUsecase>(() => LogoutUsecase(instance()));
+  }
+  // google sign in usecase
+  if (!GetIt.I.isRegistered<GoogleSignInUseCase>()) {
+    instance.registerFactory<GoogleSignInUseCase>(
+        () => GoogleSignInUseCase(instance()));
+  }
+  // forgot password usecase
+  if (!GetIt.I.isRegistered<ForgotPasswordUsecase>()) {
+    instance.registerFactory<ForgotPasswordUsecase>(
+        () => ForgotPasswordUsecase(instance()));
+  }
 }
