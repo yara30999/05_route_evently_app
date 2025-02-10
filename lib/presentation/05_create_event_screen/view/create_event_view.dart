64dff28f16b4496ef8227542aed975d843c2d7bb
yara30999 <yaranaserelniny@gmail.com>
2025-02-10 11:28:34 +0200

    install bloc.

--- lib/presentation/05_create_event_screen/view/create_event_view.dart
+++ lib/presentation/05_create_event_screen/view/create_event_view.dart
@@ -1,10 +1,31 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+
+import '../../resourses/assets_manager.dart';
 
 class CreateEventView extends StatelessWidget {
   const CreateEventView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("create_event".tr()),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(25),
+              child: Image.asset(
+                PngAssets.birthday,
+                height: 200,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
